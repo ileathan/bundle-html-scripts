d1b456daebb4e43b9f549f3fb32f8d63e04d7e8c
leathan <1@leathan.xyz>
2017-08-05 12:12:17 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bundle and compress all scripts from any local or remote server's html file
---
+++ bundle-html-scripts.js
@@ -0,0 +1,42 @@
+// Extracts all script src's from an html file
+// then minifies, and concatenates them into one file.
+// Author: leathan
+// License: MIT
+process.argv[2] && ExtractBundle(process.argv[2]);
+module.exports = ExtractBundle;
+function ExtractBundle(file, verbose) {
+  const { minify } = require("uglify-es");
+  const request = require('request')
+  const fs = require('fs')
+  host = /(.*)\//.exec(file)[1];
+  !file && !console.log("Provide a link.") && process.exit(1)
+
+  const results = [];
+  var found = 0;
+
+  // Probably wasted my time with this one, but if their is no new data found in 15 seconds,
+  // atleast 1 server hung, so check and exit if lastfound(15 seconds ago) === found(nowish).
+  (function forever(lastfound) {
+    setTimeout(()=>{ if(found === lastfound) !console.log("Atleast 1 server hung.") && process.exit(1); else forever(found) }, 15000)
+  })();
+
+  request(file, (_, __, data) => {
+    !data && !console.log("No data from link.") && process.exit(1)
+    const sources = data.replace(/<!--.*-->/g,'') // Remove comments
+    .match(/<script.*?>.*<\/script>/mg)               // Get all lines with scripts
+    .map(_=>_.match(/src\s*=\s*"(.*)"/)[1]) // Return array of source locations.
+    sources.map((source, i, arr) => {
+      const url = /\/\//.test(source) ? source : host + source;
+      (process.argv[3] || verbose) && !console.log("fetching " + url)
+      request(url, (err, res, body) => {
+        results[i] = minify(body).code || {error: true}
+        if(results[i].error) !console.log("Error parsing " + url) && process.exit(1)
+        if(++found === arr.length) {
+          fs.writeFileSync('bundle.js', results.join(''))
+          console.log('Saved output to ./bundle.js')
+          process.argv[2] && process.exit(0);
+        }
+      })
+    })
+  })
+}
